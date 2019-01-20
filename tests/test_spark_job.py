499051e86929dce28372e4a88a4df0f13a8dac92
Wolfgang Vogl <wolfgang.vogl01@gmail.com>
2019-01-20 23:21:02 +0100

    fix/File-structure-of-tests

--- tests/test_spark_job.py
+++ tests/test_spark_job.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import os
+import glob
 import subprocess
 import json
 
@@ -18,11 +19,11 @@ def file_path(file_name, dir=""):
     return os.path.dirname(base_path) + "/" + dir + "/" + file_name
 
 class SimpleTest(PySparkTest):
-    def test_basic_spark_behaviour(self):
-        test_rdd = self.spark.sparkContext.parallelize(['cat dog mouse','cat cat dog'], 2)
-        results = test_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(add).collect()
-        expected_results = [('cat', 3), ('dog', 2), ('mouse', 1)]
-        self.assertEqual(set(results), set(expected_results))
+    # def test_basic_spark_behaviour(self):
+    #     test_rdd = self.spark.sparkContext.parallelize(['cat dog mouse','cat cat dog'], 2)
+    #     results = test_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(add).collect()
+    #     expected_results = [('cat', 3), ('dog', 2), ('mouse', 1)]
+    #     self.assertEqual(set(results), set(expected_results))
 
     # def test_read_file(self):
     #     shortage = "VOW.DE"
@@ -32,12 +33,22 @@ class SimpleTest(PySparkTest):
     #     input_data = obj.read_file(self.spark, input_file, file_format)
     #     result = input_data.select("*")
 
+    def test_get_stock_data_from_file(self):
+        use_case_objects = []
+        data = {}
+        file_paths = glob.glob("./tests/fixtures/input/*.csv")
+        data["shortages"] = [p[p.rfind("/")+1   :p.rfind(".csv")] for p in file_paths]
+        for shortage in data["shortages"]:
+            use_case_objects.append(ExampleUseCase(spark_session=self.spark, shortage=shortage))
+        [data.get_stock_data_from_file_source() for data in use_case_objects]
+        [data.predict_low_cost_high_value() for data in use_case_objects]
+
+
     # @patch('alfred_aftersales_uc3_pickerl.read_file', return_value=self.spark.createDataFrame())
     # @patch('example_use_case.ExampleUseCase.write_file', return_value=None)
     # @patch('example_use_case.ExampleUseCase.draw_result', return_value=None)
     # def test_example_use_case(self, draw_result):
-
-    def test_example_use_case(self):
+    def test_get_stock_data_from_web(self):
         use_case_objects = []
         file_p = file_path("shortages.json", "tests/fixtures")
         with open(file_p) as f:
@@ -45,6 +56,16 @@ class SimpleTest(PySparkTest):
         for shortage in data["shortages"]:
             use_case_objects.append(ExampleUseCase(spark_session=self.spark, shortage=shortage))
         [data.get_stock_data_from_web_source() for data in use_case_objects]
-        # [data.get_stock_data_from_file_source() for data in use_case_objects]
         [data.predict_low_cost_high_value() for data in use_case_objects]
-        # [data.draw_result() for data in use_case_objects]
+
+    # def test_draw_result(self):
+    #     use_case_objects = []
+    #     data = {}
+    #     file_paths = glob.glob("./tests/fixtures/input/*.csv")
+    #     data["shortages"] = [p[p.rfind("/")+1   :p.rfind(".csv")] for p in file_paths]
+    #     for shortage in data["shortages"]:
+    #         use_case_objects.append(ExampleUseCase(spark_session=self.spark, shortage=shortage))
+    #     [data.get_stock_data_from_file_source() for data in use_case_objects]
+    #     [data.predict_low_cost_high_value() for data in use_case_objects]
+    #     # [data.draw_result() for data in use_case_objects]
+    # 
